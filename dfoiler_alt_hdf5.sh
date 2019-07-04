4ec715ac6d1dbeee06dd6d5f17ae72040cf41ce8
Bradley Martin <evobio721@gmail.com>
2019-07-04 00:19:07 -0500

    removed write pre-check to file

--- dfoiler_alt_hdf5.sh
+++ dfoiler_alt_hdf5.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p dfoil
 mkdir -p precheck
-find ./counts/ -type f -name '*.tab.h5' | parallel python dfoil_hdf5.py -H --mode dfoilalt --infile {} --out dfoil/{/.}.dfoil_alt ">" precheck/{/.}.precheck_alt
+find ./counts/ -type f -name '*.tab.h5' | parallel python dfoil_hdf5.py -H --mode dfoilalt --infile {} --out dfoil/{/.}.dfoil_alt
